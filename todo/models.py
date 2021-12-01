525cf3cb947d414d3950b299bce385ea516fee41
LaraArias <82562682+LaraArias@users.noreply.github.com>
2021-12-01 12:56:31 -0600

    add date

--- todo/models.py
+++ todo/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime
+from django.utils.timezone import now
 
 # Create your models here.
 
@@ -6,7 +8,7 @@ class Todo(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
-    date_due = models.DateTimeField()
+    date_due = models.DateTimeField(default=now, editable=False)
 
     def _str_(self):
         return self.title
