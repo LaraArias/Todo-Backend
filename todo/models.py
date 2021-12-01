d37ba4c1fba8cc943ac7282ab8440429191a8db7
LaraArias <82562682+LaraArias@users.noreply.github.com>
2021-12-01 12:39:43 -0600

    cambios

--- todo/models.py
+++ todo/models.py
@@ -6,6 +6,7 @@ class Todo(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
+    date_due = models.DateTimeField()
 
     def _str_(self):
         return self.title
